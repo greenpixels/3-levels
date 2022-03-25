26992bd857f447a5496b336ba62ba1ecba045b30
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2022-03-25 11:19:27 +0100

    Remove custom headers on request
    CORS-Preflight will return 403 since GitHub does not serve OPTIONS-Requests. With custom headers removed, CORS will not be triggered and the request is successful.
    
    More here:
    https://web.dev/cross-origin-resource-sharing/?utm_source=devtools#preflight-requests-for-complex-http-calls
    
    https://stackoverflow.com/a/16945321

--- HTTPGetHighscores.gd
+++ HTTPGetHighscores.gd
@@ -17,5 +17,5 @@
 extends HTTPRequest
 
 func _ready():
-	var _is_ok : bool = request("https://raw.githubusercontent.com/greenpixels/3Levels/main/highscores.json", ["content-type: application/json"]);
+	var _is_ok : bool = request("https://raw.githubusercontent.com/greenpixels/3Levels/main/highscores.json", []);
 	
